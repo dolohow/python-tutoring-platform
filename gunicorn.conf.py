255f2e1e374f3d33e1a5a45b3f0b8864337f1a6d
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2025-05-29 12:30:35 +0200

    gunicorn: Use sync workers
    This could cause issues with dying postgres connections

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,8 +2,8 @@ import multiprocessing
 
 bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gthread"
-threads = 2  # each worker gets N Python threads
+worker_class = "sync"
+# threads = 2  # each worker gets N Python threads
 preload_app = True
 timeout = 30  # kill hung requests
 graceful_timeout = 30
